ab24024acd788f6e41a7e48c21daf3f7c0b33d76
Moritz Roessler <morrossl@publicisgroupe.net>
2022-12-22 23:21:06 +0100

    fix: remove localtunnel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import localtunnel from 'vite-plugin-localtunnel';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	https: true,
-	plugins: [
-		sveltekit(),
-		localtunnel({
-			subdomain: null // set or set null for a random subdomain
-		})
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
